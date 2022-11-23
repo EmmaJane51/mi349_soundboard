73b665e8f4b7adbb396a00fdff8aa4db161dae62
Emma Andrews <emmaandrews@Emmas-MacBook-Pro.local>
2022-11-23 18:06:27 -0500

    didnt work

--- soundboard.js
+++ soundboard.js
@@ -16,11 +16,3 @@ document.onclick = function() {
   audio.play();
 }
 
-var sound1 = new Audio();
-sound1.src = "applause_sound.wav";
-
-var sound2 = new Audio();
-sound2.src = "doorbell_sound.wav";
-
-var sound3 = new Audio();
-sound3.src = "laser_sound.wav";
