3c15fb31907bc68c846262b79b5ed9619d99be33
Emma Andrews <emmaandrews@Emmas-MacBook-Pro.local>
2022-11-23 18:04:30 -0500

    fixing sounds

--- soundboard.js
+++ soundboard.js
@@ -15,3 +15,12 @@ var audio = new Audio("laser_sound.wav");
 document.onclick = function() {
   audio.play();
 }
+
+var sound1 = new Audio();
+sound1.src = "applause_sound.wav";
+
+var sound2 = new Audio();
+sound2.src = "doorbell_sound.wav";
+
+var sound3 = new Audio();
+sound3.src = "laser_sound.wav";
