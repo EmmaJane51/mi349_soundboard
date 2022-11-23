865c36c7f9de5ab9c00d430212c3cbc352491b15
Emma Andrews <emmaandrews@Emmas-MacBook-Pro.local>
2022-11-22 19:06:14 -0500

    adding sounds

--- soundboard.js
+++ soundboard.js
@@ -3,3 +3,15 @@ var audio = new Audio("applause_sound.wav");
 document.onclick = function() {
   audio.play();
 }
+
+var audio = new Audio("doorbell_sound.wav");
+
+document.onclick = function() {
+  audio.play();
+}
+
+var audio = new Audio("laser_sound.wav");
+
+document.onclick = function() {
+  audio.play();
+}
